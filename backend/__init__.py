840edd102d96c7f1dc3ec70b7b175cee163b3fda
pranavjoy <pranavjoy@protonmail.com>
2017-10-30 14:30:53 +0100

    fial

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,4 +1,7 @@
 def process_user_query(query_string):
     name_list = query_string.split(' ')
-    result = 'Hi There, Pranav \nHi There, Joy'
-    return result
+    greetings = []
+    for name in name_list:
+        greetings.append()
+    # result = 'Hi There, Pranav \nHi There, Joy'
+    return greetings
