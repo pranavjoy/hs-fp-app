58ea1e3588862f3b8e1ac371332ac7ffb0755d27
pranavjoy <pranavjoy@protonmail.com>
2017-10-30 16:06:14 +0100

    finname

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,6 +2,7 @@ def process_user_query(query_string):
     name_list = query_string.split(' ')
     greetings = []
     for name in name_list:
-        greetings.append(f'Hi {name}!')
-    # result = 'Hi There, Pranav \nHi There, Joy'
+        if (name_list[0].isupper()==True):
+            finname.append(name_list)
+        greetings.append(f'Hi {finnamne}!')
     return greetings
