32665bc5cee2b47495fbedb0bd8bdff735371d8e
pranavjoy <pranavjoy@protonmail.com>
2017-10-30 14:18:09 +0100

    changes3

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,4 +1,15 @@
+#split query
 def process_user_query(query_string):
     name_list = query_string.split(' ')
-    result = f'Hi There, {name_list}'
-    return result
+
+#convert split query to string
+
+def convert_to_string(student_names):
+    result =''
+    for name in name_list:
+        result = result + ' ' + name
+
+#add greeting to split query
+name_list2 = convert_to_string(student_names)
+result = f'Hi There, {name_list2}'
+return result
