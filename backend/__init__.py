ec9e19cab72f9cf70af6b426507b64bfb40e5802
pranavjoy <pranavjoy@protonmail.com>
2017-10-31 15:58:18 +0100

    changes22

--- backend/__init__.py
+++ backend/__init__.py
@@ -6,7 +6,7 @@ def process_user_query(query_string):
     for name in name_list:
         if (name[0].isupper()==True):
             greetings.append(f'Hi {name}!\n')
-    return greetings
-    return 'No names found'
     url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
     webbrowser.open_new(url)
+    return greetings
+    return 'No names found'
