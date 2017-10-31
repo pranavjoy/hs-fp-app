a709b8eed0af0b4e5facc08999bb0252d505549e
pranavjoy <pranavjoy@protonmail.com>
2017-10-31 15:44:27 +0100

    changes

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,8 +1,12 @@
+import urllib
 def process_user_query(query_string):
     name_list = query_string.split(' ')
     greetings = []
     for name in name_list:
-        if (name_list[0].isupper()==True):
-            finname.append(name_list)
-        greetings.append(f'Hi {finnamne}!')
+        if (name[0].isupper()==True):
+            greetings.append(f'Hi {name}!\n')
     return greetings
+    return 'No names found'
+
+fun open():
+    return urllib.urlopen('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
