18a7489826ac3e5a6939356bf1ee3351504368a1
pranavjoy <pranavjoy@protonmail.com>
2017-11-01 14:19:05 +0100

    changes to game 1

--- backend/__init__.py
+++ backend/__init__.py
@@ -11,6 +11,10 @@
 # url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
 # webbrowser.open_new(url)
 
+"""Hangman
+Welcome to Hangman. A word is chosen at random from a list and the
+user must guess the word letter by letter before running out of attempts."""
+
 import random
 
 def main():
@@ -19,14 +23,15 @@ def main():
                'before you run out of attempts. Good luck!'
                ]
 
+    for line in welcome:
+        print(line, sep='\n')
 
-
-    # setting up the play_again loop
+    
 
     play_again = True
 
     while play_again:
-        # set up the game loop
+
 
         words = ["hangman", "chairs", "backpack", "bodywash", "clothing",
                  "computer", "python", "program", "glasses", "sweatshirt",
@@ -35,12 +40,12 @@ def main():
                  ]
 
         chosen_word = random.choice(words).lower()
-        player_guess = None # will hold the players guess
-        guessed_letters = [] # a list of letters guessed so far
+        player_guess = None
+        guessed_letters = []
         word_guessed = []
         for letter in chosen_word:
-            word_guessed.append("-") # create an unguessed, blank version of the word
-        joined_word = None # joins the words in the list word_guessed
+            word_guessed.append("-")
+        joined_word = None
 
         HANGMAN = (
 """
@@ -176,29 +181,29 @@ def main():
 --------
 """)
 
-        return HANGMAN[0]
+        print(HANGMAN[0])
         attempts = len(HANGMAN) - 1
 
 
         while (attempts != 0 and "-" in word_guessed):
-            return(("\nYou have {} attempts remaining").format(attempts))
+            print(("\nYou have {} attempts remaining").format(attempts))
             joined_word = "".join(word_guessed)
-            return(joined_word)
+            print(joined_word)
 
             try:
-                player_guess = str(query_string("\nPlease select a letter between A-Z" + "\n> ")).lower()
-            except: # check valid input
-                return("That is not valid input. Please try again.")
+                player_guess = str(input("\nPlease select a letter between A-Z" + "\n> ")).lower()
+            except:
+                print("That is not valid input. Please try again.")
                 continue
             else:
-                if not player_guess.isalpha(): # check the input is a letter. Also checks an input has been made.
-                    return("That is not a letter. Please try again.")
+                if not player_guess.isalpha():
+                    print("That is not a letter. Please try again.")
                     continue
-                elif len(player_guess) > 1: # check the input is only one letter
-                    return("That is more than one letter. Please try again.")
+                elif len(player_guess) > 1:
+                    print("That is more than one letter. Please try again.")
                     continue
-                elif player_guess in guessed_letters: # check it letter hasn't been guessed already
-                    return("You have already guessed that letter. Please try again.")
+                elif player_guess in guessed_letters:
+                    print("You have already guessed that letter. Please try again.")
                     continue
                 else:
                     pass
@@ -207,20 +212,20 @@ def main():
 
             for letter in range(len(chosen_word)):
                 if player_guess == chosen_word[letter]:
-                    word_guessed[letter] = player_guess # replace all letters in the chosen word that match the players guess
+                    word_guessed[letter] = player_guess
 
             if player_guess not in chosen_word:
                 attempts -= 1
-                return(HANGMAN[(len(HANGMAN) - 1) - attempts])
+                print(HANGMAN[(len(HANGMAN) - 1) - attempts])
 
-        if "-" not in word_guessed: # no blanks remaining
-            return(("\nCongratulations! {} was the word").format(chosen_word))
-        else: # loop must have ended because attempts reached 0
-            return(("\nUnlucky! The word was {}.").format(chosen_word))
+        if "-" not in word_guessed:
+            print(("\nCongratulations! {} was the word").format(chosen_word))
+        else:
+            print(("\nUnlucky! The word was {}.").format(chosen_word))
 
-        return("\nWould you like to play again?")
+        print("\nWould you like to play again?")
 
-        response = query_string("> ").lower()
+        response = input("> ").lower()
         if response not in ("yes", "y"):
             play_again = False
 
